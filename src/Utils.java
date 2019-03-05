ffd44b7cfe4e3355f6e2caae643e296de94d3255
Zoe Chevalier <zoe.l.chevalier@gmail.com>
2019-03-04 17:05:13 -0800

    created method parse2016PresidentialResults

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -18,5 +19,18 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResults> parse2016PresidentialResults(String data){
+        ArrayList<ElectionResults> output = new ArrayList<>();
+        String [] rows = data.split("\n");
+
+        for(int i = 1; i < rows.length; i ++){
+            String [] a = rows[i].split(",");
+            ElectionResults result = new ElectionResults(Integer.parseInt(a[0]), (double)Integer.parseInt(a[1]), (double)Integer.parseInt(a[2]),
+                    Integer.parseInt(a[3]), (double)Integer.parseInt(a[4]), (double)Integer.parseInt(a[5]), a[6],
+                    (double)Integer.parseInt(a[7]), a[8], Integer.parseInt(a[9]));
+            output.add(result);
+        } return output;
     }
 }
